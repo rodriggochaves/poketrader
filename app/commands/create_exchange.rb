9a41cc89405eaa59ad34f83e7e7f712737ce7201
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2021-01-09 20:21:29 -0300

    refactor: create generic method to both exchange sides

--- app/commands/create_exchange.rb
+++ app/commands/create_exchange.rb
@@ -1,23 +1,28 @@
 class CreateExchange
+  attr_reader :exchange, :left_side, :right_side
+
   def initialize(params)
     @left_side = params[:left]
     @right_side = params[:right]
   end
 
   def call
-    exchange = Exchange.create
+    @exchange = Exchange.create
     exchange_pokemons(exchange)
   end
 
+  private
+
   def exchange_pokemons(exchange)
-    left_pokemons = @left_side.map do |pokemon_name|
-      pokemon = FindOrFetchPokemon.new(pokemon_name).call
-      ExchangedPokemon.create(pokemon: pokemon, side: ExchangedPokemon.sides[:left], exchange: exchange)
-    end
-    right_pokemons = @right_side.map do |pokemon_name|
-      pokemon = FindOrFetchPokemon.new(pokemon_name).call
-      ExchangedPokemon.create(pokemon: pokemon, side: ExchangedPokemon.sides[:right], exchange: exchange)
-    end
+    left_pokemons = exchange_side(left_side, ExchangedPokemon.sides[:left])
+    right_pokemons = exchange_side(right_side, ExchangedPokemon.sides[:right])
     left_pokemons + right_pokemons
+  end
+
+  def exchange_side(pokemon_names, side)
+    pokemon_names.map do |pokemon_name|
+      pokemon = FindOrFetchPokemon.new(pokemon_name).call
+      ExchangedPokemon.create(pokemon: pokemon, side: side, exchange: exchange)
+    end
   end
 end
