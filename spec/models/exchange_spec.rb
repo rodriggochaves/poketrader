afd221ac68402e40cdf245103f28f8d3b27034ad
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2021-01-09 19:46:15 -0300

    feat: add association method
    add exchange has many exchanged_pokemons method

--- spec/models/exchange_spec.rb
+++ spec/models/exchange_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Exchange, type: :model do
+  it "has many exchanged pokemons" do
+    exchange = create(:exchange)
+    pokemons = [create(:charmander), create(:bulbasaur)]
+    pokemons.map { |pokemon| create(:exchanged_pokemon, exchange: exchange, pokemon: pokemon) }
+
+    expect(exchange.reload.exchanged_pokemons.map(&:class).uniq).to eq([ExchangedPokemon])
+  end
 end
