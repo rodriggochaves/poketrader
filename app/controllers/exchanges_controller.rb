5af2a41eb14769e5af3098d022310fd6d5f6e172
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2021-01-10 13:13:21 -0300

    feat: send flash message about fairness

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -4,15 +4,23 @@ class ExchangesController < ApplicationController
   def index; end
 
   def create
-    CreateExchange.new(exchange_params).call
-    redirect_to new_exchange_path, notice: "Your Pokemons was sent successfully"
+    @exchange = CreateExchange.new(exchange_params).call
+    redirect_to new_exchange_path, fairness_message
   rescue FindOrFetchPokemon::PokemonNotFound => exception
-    redirect_to new_exchange_path, alert: exception.message
+    redirect_to new_exchange_path, flash: { error: exception.message }
   end
 
   private
 
   def exchange_params
     params.permit(left: [], right: [])
+  end
+
+  def fairness_message
+    if @exchange.fair?
+      { notice: "Pokemons successfully exchanged\nIt was a fair trade!" }
+    else
+      { alert: "Pokemons successfully exchanged\nIt wasn't a fair trade!" }
+    end
   end
 end
--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 RSpec.describe "Exchanges", type: :request do
   describe "POST /exchanges" do
     subject(:create_exchange_request) do
-      post "/exchanges", params: { left: [bulbasaur.name], right: [charmander.name] }
+      post "/exchanges", params: params
     end
 
     let(:bulbasaur) { create(:bulbasaur) }
     let(:charmander) { create(:charmander) }
+    let(:mewtwo) { create(:pokemon, id: 150, name: "mewtwo", base_experience: 306) }
+    let(:params) { { left: [bulbasaur.name], right: [charmander.name] } }
 
     context "when pokemon names are found" do
       it "responds with HTTP 302" do
@@ -15,13 +17,22 @@ RSpec.describe "Exchanges", type: :request do
         expect(response).to have_http_status(302)
       end
 
-      it "sends a notice flash" do
-        create_exchange_request
-        expect(flash[:notice]).to eq("Your Pokemons was sent successfully")
-      end
-
       it "creates a new exchange" do
         expect { create_exchange_request }.to change(Exchange, :count).by(1)
+      end
+
+      it "sends a flash message if the exchange was fair" do
+        create_exchange_request
+        expect(flash[:notice]).to eq("Pokemons successfully exchanged\nIt was a fair trade!")
+      end
+
+      context "if the exchange was not fair" do
+        let(:params) { { left: [bulbasaur.name], right: [mewtwo.name] } }
+
+        it "sends a flash alert message " do
+          create_exchange_request
+          expect(flash[:alert]).to eq("Pokemons successfully exchanged\nIt wasn't a fair trade!")
+        end
       end
     end
 
@@ -42,12 +53,15 @@ RSpec.describe "Exchanges", type: :request do
 
       it "sends an alert flash" do
         create_exchange_request
-        expect(flash[:alert]).to eq("'Chamander' is not a pokemon")
+        expect(flash[:error]).to eq("'Chamander' is not a pokemon")
       end
 
       it "expects to not create a new exchange" do
         expect { create_exchange_request }.not_to change(Exchange, :count)
       end
+    end
+
+    context "when there is a unknown error" do
     end
   end
 end
