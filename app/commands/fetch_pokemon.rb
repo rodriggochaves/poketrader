44bbe9bac21595e6388dff6f26fcc19997ae82f9
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2021-01-12 07:50:01 -0300

    refactor: extract fetch_pokemon command

---
+++ app/commands/fetch_pokemon.rb
@@ -0,0 +1,36 @@
+class FetchPokemon
+  class PokemonNotFound < StandardError
+  end
+
+  attr_reader :name, :poke_api, :pokemon_data
+
+  def initialize(name:, api: PokeApi)
+    @name = name
+    @poke_api = api
+  end
+
+  def call
+    @pokemon_data = poke_api.get(pokemon: name)
+    raise PokemonNotFound, "#{name} is not a pokemon" unless pokemon_data.name.present?
+
+    build_pokemon
+  rescue JSON::ParserError => exception
+    handle_error(exception)
+  end
+
+  private
+
+  def build_pokemon
+    Pokemon.new(
+      poke_index: pokemon_data.id,
+      name: pokemon_data.name,
+      base_experience: pokemon_data.base_experience
+    )
+  end
+
+  def handle_error(exception)
+    raise PokemonNotFound, "#{name} is not a pokemon" if exception.message.scan(/not found/i)
+
+    raise exception
+  end
+end
---
+++ spec/commands/fetch_pokemon_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe FetchPokemon do
+  subject(:fetch_pokemon) { FetchPokemon.new(name: name, api: api).call }
+
+  let(:name) { "groudon" }
+  let(:api) { double("Poke API", get: pokemon) }
+  let(:pokemon) { double(id: 1, name: name, base_experience: 302, poke_index: 383) }
+
+  context "when querying a valid name" do
+    it "expects to call PokeApi with the pokemon name" do
+      expect(api).to receive(:get).with(pokemon: name)
+      fetch_pokemon
+    end
+
+    it "returns an instance of Pokemon" do
+      expect(fetch_pokemon).to be_instance_of(Pokemon)
+    end
+  end
+
+  context "when querying a empty string" do
+    let(:name) { "" }
+    let(:pokemon) { double(id: 1, name: nil) }
+
+    it "raises an error PokemonNotFound" do
+      expect { fetch_pokemon }.to raise_error(FetchPokemon::PokemonNotFound)
+    end
+  end
+
+  context "when querying a invalid name" do
+    let(:name) { "pudim" }
+
+    before do
+      allow(api).to receive(:get).and_raise(JSON::ParserError)
+    end
+
+    it "raises an error PokemonNotFound" do
+      expect { fetch_pokemon }.to raise_error(FetchPokemon::PokemonNotFound)
+    end
+  end
+
+  context "when an unknown error happens" do
+    before do
+      allow(api).to receive(:get).and_raise(StandardError.new("unknown error"))
+    end
+
+    it "expects to raise a PokemonNotFound exception" do
+      expect { fetch_pokemon }.to raise_error(StandardError)
+    end
+  end
+end
