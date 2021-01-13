4f80776f9ec4800f480e12edba24e12669c7ae03
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2021-01-13 13:47:13 -0300

    fix: remove pending jquery import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import $ from "jquery";
-
-import setupPokemonInputs from "./setup-pokemon-inputs";
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 // Rails.start()
 // Turbolinks.start()
