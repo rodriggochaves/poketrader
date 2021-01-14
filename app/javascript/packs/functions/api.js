69a24610259aa7ccd47f8cf3c02b4aed00204a8c
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2021-01-14 20:08:47 -0300

    refactor: extract post function

--- app/javascript/packs/functions/api.js
+++ app/javascript/packs/functions/api.js
@@ -4,31 +4,28 @@ export async function searchPokemons(query) {
   return response.json();
 }
 
-export async function postExchange(leftPokemons, rightPokemons) {
-  await fetch("/exchanges", {
+function post(url, body) {
+  return fetch(url, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      left: leftPokemons,
-      right: rightPokemons,
-    }),
+    body: JSON.stringify(body),
+  });
+}
+
+export async function postExchange(leftPokemons, rightPokemons) {
+  await post("/exchanges", {
+    left: leftPokemons,
+    right: rightPokemons,
   });
 }
 
 export async function postFairness(leftPokemons, rightPokemons) {
-  const response = await fetch("/exchanges/simulate", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      left: leftPokemons,
-      right: rightPokemons,
-    }),
+  const response = await post("/exchanges/simulate", {
+    left: leftPokemons,
+    right: rightPokemons,
   });
 
   return response.json();
